8687f2905ec8701e1927d48f0d0235591954fa93
Priyanka <priyankahansi95@gmail.com>
2022-04-21 15:33:39 +0530

    added the initial position

--- src/com/bridgelabz/Snake_Ladder.java
+++ src/com/bridgelabz/Snake_Ladder.java
@@ -2,6 +2,7 @@ package com.bridgelabz;
 
 public class Snake_Ladder {
     public static void main(String[] args) {
-        System.out.println("welcome to snake and ladder game");
+        int position = 0;
+        System.out.println("the initial position of player one:" + position);
     }
 }
