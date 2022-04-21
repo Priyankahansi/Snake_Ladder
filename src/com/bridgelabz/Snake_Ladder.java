62d721ccdd2686350859c374e4ac69a2a2ed9196
Priyanka <priyankahansi95@gmail.com>
2022-04-21 15:53:06 +0530

    added to get random number one to six

--- src/com/bridgelabz/Snake_Ladder.java
+++ src/com/bridgelabz/Snake_Ladder.java
@@ -1,8 +1,15 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class Snake_Ladder {
+    static void getDieRoll() {
+        Random random = new Random();
+        int roll = random.nextInt(6) + 1;
+        System.out.println(roll);
+    }
+
     public static void main(String[] args) {
-        int position = 0;
-        System.out.println("the initial position of player one:" + position);
+        getDieRoll();
     }
 }
